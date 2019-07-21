c88e76061198bb66b39433ea10ddba3b50451d38
Noreply <noreply@github.com>
2019-07-21 21:17:44 +0200

    Change plotit to accept new data

--- plotit.py
+++ plotit.py
@@ -2,7 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv("thmsteps.csv", sep="\t", header=None, names=["thm", "steps", "subtheorems", "totalsteps", "expandedsteps"])#remove 1
+names = "thm, steps, subtheorems_dup, totalsteps_dup, subtheorems, totalsteps, expandedsteps, maxpathlength".split(", ")
+
+df = pd.read_csv("thmsteps.csv", sep="\t", header=None, names=names)#remove 1
 print(df)
 
 NSAMPLE = 20#1
@@ -12,11 +14,23 @@ df = df.sample(len(df)//NSAMPLE)
 X = "expandedsteps"
 Y = "subtheorems"
 
+XLOG = True
+YLOG = False
+
 #df[Y] = [len(y) for y in df[Y]]
-df[X] = np.log(df[X].astype("float"))
-#df[Y] = np.log(df[Y].astype("float"))
+if XLOG:
+    df[X] = np.log(df[X].astype("float"))
+
+if YLOG:
+    df[Y] = np.log(df[Y].astype("float"))
 
 ax = df.set_index(X)[Y].plot(style="o", markersize=1)
+
+xlabel = "log(%s)" % X if XLOG else X
+ylabel = "log(%s)" % Y if YLOG else Y
+
+ax.set_xlabel(xlabel)
+ax.set_ylabel(ylabel)
 
 def label_point(x, y, val, ax):
     a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
