b5e3406ae0bcee4a75fef78315c0e63ceda9ff67
Noreply <noreply@github.com>
2019-07-21 20:29:31 +0200

    Add log transform, numpy requirement

--- plotit.py
+++ plotit.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from math import log
+import numpy as np
 
 df = pd.read_csv("thmsteps.csv", sep="\t", header=None, names=["thm", "steps", "subtheorems", "totalsteps", "expandedsteps"])#remove 1
 print(df)
 
-ax = df.set_index("totalsteps")["subtheorems"].plot(style="o")
+df = df.sample(len(df))#//100
+
+X = "expandedsteps"
+Y = "subtheorems"
+
+df[X] = np.log(df[X].astype("float"))
+
+ax = df.set_index(X)[Y].plot(style="o", markersize=1)
 
 def label_point(x, y, val, ax):
     a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
     for i, point in a.iterrows():
         ax.text(point['x'], point['y'], str(point['val']))
 
-label_point(df.totalsteps, df.subtheorems, df.thm, ax)
+#label_point(df[X], df[Y], df.thm, ax)
 #plt.savefig("figure.png")
 plt.show()
